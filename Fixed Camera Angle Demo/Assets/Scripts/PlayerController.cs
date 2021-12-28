2c791cc56e332dcc304e516a54dddf4e84cbbf72
Saul Carreon <sacarreon18@gmail.com>
2021-12-28 03:45:59 -0600

    Implemented Animation
    imported player model and animation assets, and created a working moveable character

--- Fixed Camera Angle Demo/Assets/Scripts/PlayerController.cs
+++ Fixed Camera Angle Demo/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] private float playerSpeed = 5f;
-    [SerializeField] private float rotationSpeed = 50f;
+    [SerializeField] private float rotationSpeed = 60f;
     [SerializeField] private Camera mainCam;
+    [SerializeField] private Animator anim;
+    [SerializeField] private bool isWalking;
+    [SerializeField] private bool isTurning;
     private int numCamLocations;
     public GameObject camParent;
     public Transform[] camLocations;
@@ -15,6 +18,12 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (!anim)
+            anim = GetComponent<Animator>();
+
+        isWalking = false;
+        isTurning = false;
+
         rb = GetComponent<Rigidbody>();
         mainCam = Camera.main;
         camParent = GameObject.FindGameObjectWithTag("CameraLocations");
@@ -22,7 +31,6 @@ public class PlayerController : MonoBehaviour
         camLocations = new Transform[numCamLocations];
         for (int i = 0; i < numCamLocations; i++)
         {
-            Debug.Log(i);
             camLocations[i] = camParent.transform.GetChild(i);
         }
     }
@@ -33,33 +41,62 @@ public class PlayerController : MonoBehaviour
         float playerMovement = Input.GetAxis("Vertical");
         float playerRotation = Input.GetAxis("Horizontal");
 
-        transform.Translate(0, 0, playerSpeed * playerMovement * Time.deltaTime);
+        transform.Translate(0, 0, playerSpeed * (playerMovement / 4f) * Time.deltaTime);
         transform.Rotate(0, playerRotation * rotationSpeed * Time.deltaTime, 0);
+
+        if (playerMovement != 0)
+            isWalking = true;
+        else
+            isWalking = false;
+
+        if (isWalking == false)
+            anim.SetBool("isWalking", false);
+
+        if (isWalking == true)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("walkSpeed", Input.GetAxis("Vertical") / 4f);
+        }
+
+        if (playerRotation != 0)
+            isTurning = true;
+        else
+            isTurning = false;
+
+        if (isTurning == false)
+            anim.SetBool("isTurning", false);
+
+        if (isTurning == true)
+        {
+            anim.SetBool("isTurning", true);
+            anim.SetFloat("turnSpeed", Input.GetAxis("Horizontal"));
+        }
+
+        if (isTurning == true && isWalking == true)
+        {
+            anim.SetFloat("turnSpeed", 0);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Cam1Collider")
         {
-            Debug.Log("Turn on cam 1");
             mainCam.transform.position = camLocations[0].position;
             mainCam.transform.rotation = camLocations[0].rotation;
         }
         if (other.gameObject.name == "Cam2Collider")
         {
-            Debug.Log("Turn on cam 2");
             mainCam.transform.position = camLocations[1].position;
             mainCam.transform.rotation = camLocations[1].rotation;
         }
         if (other.gameObject.name == "Cam3Collider")
         {
-            Debug.Log("Turn on cam 3");
             mainCam.transform.position = camLocations[2].position;
             mainCam.transform.rotation = camLocations[2].rotation;
         }
         if (other.gameObject.name == "Cam4Collider")
         {
-            Debug.Log("Turn on cam 4");
             mainCam.transform.position = camLocations[3].position;
             mainCam.transform.rotation = camLocations[3].rotation;
         }
